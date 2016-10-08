0cef1a4596f0be6e36ad67b8b4ae3985a007586e
xiarui@maianer.com <xiarui@mainaer.com>
2016-10-08 15:25:18 +0800

    add sensor

--- app/src/main/java/com/harry/video/VideoActivity.java
+++ app/src/main/java/com/harry/video/VideoActivity.java
@@ -3,7 +3,8 @@
  */
 package com.harry.video;
 
-import android.net.Uri;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -24,15 +25,27 @@ public class VideoActivity extends AppCompatActivity {
     @BindView(R.id.jc_player)
     JCVideoPlayerStandard mPLayer;
 
+    JCVideoPlayer.JCAutoFullscreenListener sensorListener;
+    SensorManager mSensorManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
         ButterKnife.bind(this);
+        mSensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorListener=new JCVideoPlayer.JCAutoFullscreenListener();
         mPLayer.setUp("http://2449.vod.myqcloud.com/2449_22ca37a6ea9011e5acaaf51d105342e3.f20.mp4"
             , JCVideoPlayerStandard.SCREEN_LAYOUT_LIST, "嫂子闭眼睛");
-        mPLayer.thumbImageView.setImageURI(Uri.parse("http://p.qpic"
-            + ".cn/videoyun//02449_43b6f696980311e59ed467f22794e792_1/640"));
+//        mPLayer.thumbImageView.setImageURI(Uri.parse("http://p.qpic"
+//            + ".cn/videoyun//02449_43b6f696980311e59ed467f22794e792_1/640"));
+//        mPLayer.startWindowFullscreen();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Sensor accelerometerSensor=mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(sensorListener,accelerometerSensor,SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -47,6 +60,7 @@ public class VideoActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         JCVideoPlayer.releaseAllVideos();
+        mSensorManager.unregisterListener(sensorListener);
     }
 
 }
