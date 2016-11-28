01f048edae745cdd7ff152fd43146e9c14b58f1e
xiarui@maianer.com <xiarui@mainaer.com>
2016-11-28 16:47:10 +0800

    add immersive mode

--- app/src/main/java/com/harry/ImmersiveActivity.java
+++ app/src/main/java/com/harry/ImmersiveActivity.java
@@ -33,4 +33,21 @@ public class ImmersiveActivity extends Activity {
         }
 
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        //适合游侠模式下使用
+//        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+//            View decorView = getWindow().getDecorView();
+//            decorView.setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+//        }
+    }
+
 }
